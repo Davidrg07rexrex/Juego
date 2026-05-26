bd1a6c57fb6d1c110321fdda0173a2fbbb37d71f
Davidrg07rexrex <david.ruizgil@edu.uah.es>
2026-05-26 19:33:44 +0200

    Add files via upload

--- src/modelo/HabitacionModelo.java
+++ src/modelo/HabitacionModelo.java
@@ -2,14 +2,20 @@ package modelo;
 
 import mundo.Objeto;
 
-public interface HabitacionModelo {
+public interface HabitacionModelo extends Comparable<HabitacionModelo> {
     int getFilas();
     int getColumnas();
     boolean esTransitable(int fila, int columna);
     String getSimbolo(int fila, int columna);
     Entidad getEnemigoEn(int fila, int columna);
     void eliminarEnemigo(int fila, int columna);
-    void eliminarObjeto(int fila, int columna);   // para recoger objetos más tarde// ← nuevo
+    void eliminarObjeto(int fila, int columna);
     void moverEnemigo(Entidad enemigo, int nuevaFila, int nuevaColumna);
     Objeto getObjetoEn(int fila, int columna);
+    String getId();
+    String getDestinoPuerta(int fila, int columna);
+    boolean puertaNecesitaLlave(int fila, int columna);
+    String getIdLlavePuerta(int fila, int columna);
+    boolean esTrampa(int fila, int columna);
+    int getDanioTrampa(int fila, int columna);
 }

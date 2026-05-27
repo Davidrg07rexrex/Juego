9b8d4bbde6628b512b083fa9be72ee4332ea3ae4
PabloUAHGuadalajara <pablo.gonzalezmendez@edu.uah.es>
2026-05-27 21:48:42 +0200

    1er test JUnit corregido

--- src/test/CombateTest.java
+++ src/test/CombateTest.java
@@ -10,13 +10,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CombateTest {
 
     @Test
-    public void testAtacanteMataDefensor() {
+    public void testAtacanteReduceVida() {
         Jugador jugador = new Jugador("Héroe", 100, 50, 5, 3, new Posicion(0, 0));
-        Enemigo enemigo = new Enemigo("e1", "Goblin", 10, 5, 2, 1, 1); // 7 args
+        Enemigo enemigo = new Enemigo("e1", "Goblin", 10, 5, 2, 1, 1);
         enemigo.setPosicion(new Posicion(1, 0));
 
-        boolean muerto = Combate.resolver(jugador, enemigo);
-        assertTrue(muerto || enemigo.getVida() <= 0, "El enemigo debería haber muerto");
+        int vidaAntes = enemigo.getVida();
+        Combate.resolver(jugador, enemigo);
+        assertTrue(enemigo.getVida() <= vidaAntes, "La vida del defensor no puede aumentar tras un ataque");
     }
 
     @Test
